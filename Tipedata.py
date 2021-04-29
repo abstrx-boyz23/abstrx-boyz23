087bb69a1048856de844b341f69ef85b435fb7bf
abstrx-boyz23 <82667447+abstrx-boyz23@users.noreply.github.com>
2021-04-29 16:08:32 +0700

    Add files via upload
    Add file Tipedata.py

---
+++ Tipedata.py
@@ -0,0 +1,49 @@
+## TIPE DATA STANDAR
+
+# INTEGER
+print ("\n============INTEGER===========")
+integer = 23
+print ("nilai :",integer,"            , tipe :",type(integer))
+
+# FLOAT
+print ("\n============FLOAT===========")
+floatis = 3.2
+print ("nilai :",floatis,"           , tipe :",type(floatis))
+
+# BOOLEAN
+print ("\n============BOOLEAN===========")
+boolean = True
+print ("nilai :",boolean,"          , tipe :",type(boolean))
+
+# STRING
+print ("\n============STRING===========")
+string = "abstrx_boyz23"
+print ("nilai :",string," , tipe :",type(string))
+
+## TIPE DATA KHUSUS
+
+# Bilangan Kompleks
+
+print ("\n============COMPLEX===========")
+kompleks = complex(2,3)
+print("nilai :",kompleks, "       , tipe :",type(kompleks))
+
+## TIPE DATA LAINNYA
+
+print()
+# Octal
+data_octal = int(input("Masukan Data Octal : 
+data_octal = int(input("Masukan Data Octal : "))
+print ("Data Octal : ",oct(data_octal))
+
+
+
+
+
+
+
+
+
+
+
+
